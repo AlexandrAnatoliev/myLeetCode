2722ac004971880963e07c34eb8aac0e2705fdcc
AlexandrAnatoliev <per-1986@list.ru>
2026-04-16 14:46:28 +0300

    fix: failed first test

--- BinarySearch/SQRTx/Solution.php
+++ BinarySearch/SQRTx/Solution.php
@@ -1,36 +1,11 @@
 <?php
 class Solution {
   /**
-   * @param Integer[] $nums
-   * @param Integer $target
+   * @param Integer $x
    * @return Integer
    */
-  public function searchInsert($nums, $target) {
-    $start = 0;
-    $finish = count($nums) - 1;
-    $middle = 0;
-    $answer = 0;
-    
-    if ($target < 0) {
-      return 0;
-    }
-    if ($target > $nums[$finish]) {
-      return $finish + 1;
-    }
-    while ($start < $finish) {
-      $middle = $start + floor(($finish - $start) / 2);
-      $answer = $start;
-      if ($nums[$middle] == $target) {
-        $answer = $middle;
-        break;
-      } elseif ($nums[$middle] > $target) {
-        $finish = $middle;
-      } else {
-        $start = $middle + 1;
-        $answer = $start;
-      }
-    }
-    return $answer;
+  public function mySqrt($x) {
+    return $x;
   }
 }
 
@@ -40,35 +15,15 @@ $solution = new Solution();
 
 $testCases = [
   [
-    'nums' => [1,3,5,6],
-    'target' => 5,
+    'x' => 4,
     'expected' => 2,
     'description' => 'Example 1'
-  ],
-  [
-    'nums' => [1,3,5,6],
-    'target' => 2,
-    'expected' => 1,
-    'description' => 'Example 2'
-  ],
-  [
-    'nums' => [1,3,5,6],
-    'target' => 7,
-    'expected' => 4,
-    'description' => 'Example 3'
-  ],
-  [
-    'nums' => [1,3,5],
-    'target' => 3,
-    'expected' => 1,
-    'description' => 'Example 4'
   ],
 ];
 
 foreach ($testCases as $test) {
-  $result = $solution->searchInsert($test['nums'], $test['target']);
+  $result = $solution->mySqrt($test['x']);
   echo "Test: {$test['description']}\n";
-  echo "Input: target = {$test['target']}\n";
   echo "Expected: {$test['expected']}\n";
   echo "Result: {$result}\n";
   if ($result == $test['expected']) {
