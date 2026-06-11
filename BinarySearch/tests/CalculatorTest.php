a6527a1326a75eea3d63d4a8b6bd921137ee7c5d
Alexandr Anatoliev <per-1986@list.ru>
2026-06-11 17:43:27 +0000

    fix: add 2 test

---
+++ BinarySearch/tests/CalculatorTest.php
@@ -0,0 +1,18 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use App2\Calculator;
+
+class CalculatorTest extends TestCase
+{
+    public function testAdd()
+    {
+        $calc = new Calculator();
+        $this->assertEquals(5, $calc->add(2, 3));
+    }
+
+    public function testAddWithNegative()
+    {
+        $calc = new Calculator();
+        $this->assertEquals(0, $calc->add(-1, 1));
+    }
+}
