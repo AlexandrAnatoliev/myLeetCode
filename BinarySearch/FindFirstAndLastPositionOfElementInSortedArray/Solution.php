c0d3f9ac772b4653f4506fba831d8a09bb60dfdd
Alexandr Anatoliev <per-1986@list.ru>
2026-06-12 17:24:23 +0000

    fix: add test for getTargetIndex method

--- BinarySearch/FindFirstAndLastPositionOfElementInSortedArray/Solution.php
+++ BinarySearch/FindFirstAndLastPositionOfElementInSortedArray/Solution.php
@@ -3,13 +3,38 @@ namespace BinarySearch1;
 
 class Solution {
 
-    /**
-     * @param Integer[] $nums
-     * @param Integer $target
-     * @return Integer[]
-     */
-    function searchRange($nums, $target) {
-      return [1, 1];
-        
+  /**
+   * @param Integer[] $nums
+   * @param Integer $target
+   * @return Integer[]
+   */
+  function searchRange($nums, $target) {
+    return [1, 1];
+      
+  }
+
+  /**
+   * @param Integer[] $nums
+   * @param Integer $target
+   * @return Integer
+   */
+  function getTargetIndex($nums, $target)
+  {
+    $start  = 0;
+    $finish = count($nums) - 1;
+    $middle = 0;
+
+    while($start < $finish) {
+      $middle = $start + (int)(($finish - $start) / 2);
+      if($nums[$middle] == $target) {
+        return $middle;
+      } elseif($nums[$middle] > $target) {
+        $finish = $middle;
+      } else {
+        $start = $middle;
+      }
     }
+    return -1;
+  }
+
 }
--- BinarySearch/FindFirstAndLastPositionOfElementInSortedArray/tests/SolutionTest.php
+++ BinarySearch/FindFirstAndLastPositionOfElementInSortedArray/tests/SolutionTest.php
@@ -4,14 +4,20 @@ use BinarySearch1\Solution;
 
 class SolutionTest extends TestCase
 {
-    public function testSearchRange()
-    {
-        $solution = new Solution();
-        $nums = [5,7,7,8,8,10];
-        $output = [3,4];
-        $this->assertEquals(
-          $output,
-          $solution->searchRange($nums, 8));
-    }
+  public function testSearchRange()
+  {
+    $solution = new Solution();
+    $nums = [5,7,7,8,8,10];
+    $output = [3,4];
+    $this->assertEquals(
+      $output,
+      $solution->searchRange($nums, 8));
+  }
 
+  public function testGetTargetIndex() {
+    $solution = new Solution();
+    $nums = [1,2,3,6,7,10];
+    $this->assertEquals(
+      2, $solution->getTargetIndex($nums, 3));
+  }
 }
