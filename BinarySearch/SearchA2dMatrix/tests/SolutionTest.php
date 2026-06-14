f8803b40b9f01b9b1cb4901fb0d5e4dcd30bfb8c
Alexandr Anatoliev <per-1986@list.ru>
2026-06-14 19:34:30 +0000

    fix(SearchA2dMatrix): add first test

---
+++ BinarySearch/SearchA2dMatrix/tests/SolutionTest.php
@@ -0,0 +1,20 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use BinarySearch2\Solution;
+
+class SolutionTest extends TestCase
+{
+  public function test1SearchRange()
+  {
+    $solution = new Solution();
+    $matrix = [
+      [1,3,5,7],
+      [10,11,16,20],
+      [23,30,34,60]
+    ]; 
+    $output = true;
+    $this->assertEquals(
+      $output,
+      $solution->searchMatrix($matrix, 3));
+  }
+}
