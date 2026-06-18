6e11a6dc7a1afb109095b818336531d664f58a82
Alexandr Anatoliev <per-1986@list.ru>
2026-06-18 17:22:26 +0000

    fix(SearchA2dMatrix2): pass test for getTargetIndex method

--- BinarySearch/SearchA2dMatrix2/Solution.php
+++ BinarySearch/SearchA2dMatrix2/Solution.php
@@ -30,6 +30,12 @@ class Solution {
     $start  = 0;
     $middle = 0;
 
+    if($row[0] == $target) {
+      $answer['searched'] = true;
+      $answer['index'] = 0;
+      return $answer;
+    }
+
     while($start < $answer['index']) {
       $middle = $start + (int)(($endSearch - $start) / 2);
       if($row[$middle] == $target) {
