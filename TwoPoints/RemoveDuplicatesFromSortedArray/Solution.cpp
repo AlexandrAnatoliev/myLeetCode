08334d0af750138785503b811614132f5403a8aa
AlexandrAnatoliev <per-1986@list.ru>
2026-08-14 17:47:43 +0300

    fix(TwoPoints): passed example1 tests

--- TwoPoints/RemoveDuplicatesFromSortedArray/Solution.cpp
+++ TwoPoints/RemoveDuplicatesFromSortedArray/Solution.cpp
@@ -7,12 +7,19 @@ class Solution {
       int size = nums.size();
       int cnt = 1;
       int tmp = nums[0];
+      vector<int> sortNums = {};
+
+      sortNums.push_back(tmp);
+
       for (int i = 0; i < size; i++) {
         if (nums[i] != tmp) {
           cnt++;
           tmp = nums[i];
+          sortNums.push_back(tmp);
         }
       }
+
+      nums = sortNums;
       return cnt;
     }
 };
