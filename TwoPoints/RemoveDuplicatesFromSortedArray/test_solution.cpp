96f6e0c6e4f7c90dd6db378f07cf5ac9d0abcc04
AlexandrAnatoliev <per-1986@list.ru>
2026-08-14 17:39:20 +0300

    fix: add fail test

--- TwoPoints/RemoveDuplicatesFromSortedArray/test_solution.cpp
+++ TwoPoints/RemoveDuplicatesFromSortedArray/test_solution.cpp
@@ -6,5 +6,11 @@ using namespace std;
 TEST(SolutionTest, example1) {
   Solution solution;
   vector<int> nums = {1,1,2};
+  vector<int> expectedNums = {1,2};
+
   EXPECT_EQ(solution.removeDuplicates(nums), 2);
+  int size = solution.removeDuplicates(nums);
+  for (int i = 0; i < size; i++) {
+    EXPECT_EQ(nums[i], expectedNums[i]);
+  }
 }
