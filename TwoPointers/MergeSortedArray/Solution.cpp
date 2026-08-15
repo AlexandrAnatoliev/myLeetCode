32acead7efbc66e12384a4b15126c34cd8517428
Alexandr Anatoliev <per-1986@list.ru>
2026-08-15 13:43:44 +0000

    fix(TwoPointers): refactor Solution

--- TwoPointers/MergeSortedArray/Solution.cpp
+++ TwoPointers/MergeSortedArray/Solution.cpp
@@ -8,21 +8,12 @@ class Solution {
       vector<int> output = {};
       int ptr1 = 0;
       int ptr2 = 0;
-      int ptr = -1;
       while (ptr1 < m || ptr2 < n) {
-        if (nums1[ptr1] < nums2[ptr2]) {
-          if (ptr < 0 
-              || output[ptr] <= nums1[ptr1]) {
-            output.push_back(nums1[ptr1]);
-            ptr++;
-          }
+        if (ptr1 < m && nums1[ptr1] < nums2[ptr2]) {
+          output.push_back(nums1[ptr1]);
           ptr1++;
         } else {
-          if (ptr < 0 
-              || output[ptr] <= nums2[ptr2]) {
-            output.push_back(nums2[ptr2]);
-            ptr++;
-          }
+          output.push_back(nums2[ptr2]);
           ptr2++;
         }
       }
