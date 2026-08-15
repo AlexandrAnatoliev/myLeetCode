e55f2d82996f8c490a24c752942d13ac8c537f79
Alexandr Anatoliev <per-1986@list.ru>
2026-08-15 12:23:30 +0000

    fix(TwoPoints): adx fail test

---
+++ TwoPoints/MergeSortedArray/test_solution.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include "Solution.cpp"
+#include <vector>
+using namespace std;
+
+TEST(SolutionTest, example1) {
+  Solution solution;
+  vector<int> nums1 = {1,2,3,0,0,0};
+  int m = 3;
+  vector<int> nums2 = {2,5,6};
+  int n = 3;
+  vector<int> output  = {1,2,2,3,5,6}; 
+
+  for (int i = 0; i < n + m; i++) {
+    EXPECT_EQ(nums1[i], output[i]);
+  }
+}
+
