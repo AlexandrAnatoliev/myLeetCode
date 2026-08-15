c05b5de95712686b595bb72953266605d4744956
Alexandr Anatoliev <per-1986@list.ru>
2026-08-15 17:29:28 +0000

    fix(TwoPointers): passed example2 test

--- TwoPointers/IntersectionOfTwoArrays2/test_solution.cpp
+++ TwoPointers/IntersectionOfTwoArrays2/test_solution.cpp
@@ -16,3 +16,15 @@ TEST(SolutionTest, example1) {
   }
 }
 
+TEST(SolutionTest, example2) {
+  Solution solution;
+  vector<int> nums1 = {4,9,5};
+  vector<int> nums2 = {9,4,9,8,4};
+  vector<int> expectOutput  = {4,9}; 
+
+  vector<int> output = solution.intersect(nums1, nums2);
+  int size = output.size();
+  for (int i = 0; i < size; i++) {
+    EXPECT_EQ(output[i], expectOutput[i]);
+  }
+}
