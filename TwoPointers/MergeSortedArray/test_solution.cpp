c04f92fcb226ba712cbfa498154c57aa83a5ab1b
Alexandr Anatoliev <per-1986@list.ru>
2026-08-15 13:46:41 +0000

    fix(TwoPointers): passed example2 test

--- TwoPointers/MergeSortedArray/test_solution.cpp
+++ TwoPointers/MergeSortedArray/test_solution.cpp
@@ -17,3 +17,16 @@ TEST(SolutionTest, example1) {
   }
 }
 
+TEST(SolutionTest, example2) {
+  Solution solution;
+  vector<int> nums1 = {1};
+  int m = 1;
+  vector<int> nums2 = {};
+  int n = 0;
+  vector<int> output  = {1}; 
+
+  solution.merge(nums1, m, nums2, n);
+  for (int i = 0; i < n + m; i++) {
+    EXPECT_EQ(nums1[i], output[i]);
+  }
+}
