fa1564082113d37ce88d0ed275c3392e7226aa60
Alexandr Anatoliev <per-1986@list.ru>
2026-08-16 16:59:09 +0000

    fix(TwoSum2): passed example1 test

--- TwoPointers/TwoSum2/Solution.cpp
+++ TwoPointers/TwoSum2/Solution.cpp
@@ -5,7 +5,26 @@ class Solution {
   public:
     vector<int> twoSum(vector<int>& numbers,
         int target) {
-      vector<int> output = {1,2};
+      vector<int> output = {0,0};
+      int ptr1 = 0;
+      int ptr2 = 1;
+      int size = numbers.size();
+
+      while (ptr2 < size && ptr1 < size - 1) {
+        if (ptr1 == ptr2) {
+          ptr2++;
+        } else if (numbers[ptr1] + numbers[ptr2] == target) {
+          output[0] = ptr1 + 1;
+          output[1] = ptr2 + 1;
+          break;
+        } else if (ptr2 >= size) {
+          ptr1++; 
+        } else if (numbers[ptr1] + numbers[ptr2] < target) {
+          ptr2++;
+        } else {
+          ptr1++;
+        }
+      }
       return output;
     }
 };
