90ed06de738cb5d0964a57e888a1715099857f87
Alexandr Anatoliev <per-1986@list.ru>
2026-08-17 15:01:01 +0000

    fix(TwoSum4): run first test

---
+++ TwoPointers/TwoSum4/test_solution.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "Solution.cpp"
+using namespace std;
+
+TEST(SolutionTest, example1) {
+  Solution solution;
+  bool expectOutput = true;
+
+  EXPECT_EQ(true, expectOutput);
+}
+
