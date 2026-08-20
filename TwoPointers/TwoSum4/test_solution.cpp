229a4723ce7b859968eae9afc35d3aa2abaa084a
Alexandr Anatoliev <per-1986@list.ru>
2026-08-20 11:36:53 +0000

    fix(TwoSum4): fail example1 test

--- TwoPointers/TwoSum4/test_solution.cpp
+++ TwoPointers/TwoSum4/test_solution.cpp
@@ -5,7 +5,13 @@ using namespace std;
 TEST(SolutionTest, example1) {
   Solution solution;
   bool expectOutput = true;
+  TreeNode* node2 = new TreeNode(2);
+  TreeNode* node4 = new TreeNode(4);
+  TreeNode* node7 = new TreeNode(7);
+  TreeNode* node3 = new TreeNode(3, node2, node4);
+  TreeNode* node6 = new TreeNode(6, nullptr, node7);
+  TreeNode* node5 = new TreeNode(5, node3, node6);
 
-  EXPECT_EQ(true, expectOutput);
+  EXPECT_EQ(solution.findTarget(node5, 9), expectOutput);
 }
 
