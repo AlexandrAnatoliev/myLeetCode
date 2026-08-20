bd4e3468d8a83e494fa96e54c8b35f5d1ebfa627
Alexandr Anatoliev <per-1986@list.ru>
2026-08-20 12:40:52 +0000

    fix(TwoSum4): passed example2 test

--- TwoPointers/TwoSum4/test_solution.cpp
+++ TwoPointers/TwoSum4/test_solution.cpp
@@ -47,4 +47,23 @@ TEST(SolutionTest, test3) {
   delete node7;
 }
 
+TEST(SolutionTest, example2) {
+  Solution solution;
+  bool expectOutput = false;
+  TreeNode* node2 = new TreeNode(2);
+  TreeNode* node4 = new TreeNode(4);
+  TreeNode* node7 = new TreeNode(7);
+  TreeNode* node3 = new TreeNode(3, node2, node4);
+  TreeNode* node6 = new TreeNode(6, nullptr, node7);
+  TreeNode* node5 = new TreeNode(5, node3, node6);
+
+  EXPECT_EQ(solution.findTarget(node5, 28), expectOutput);
+
+  delete node2;
+  delete node3;
+  delete node4;
+  delete node5;
+  delete node6;
+  delete node7;
+}
 
