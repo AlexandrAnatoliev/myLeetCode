a49f8844e11b789ba13d796dbed7ec0b13f53444
Alexandr Anatoliev <per-1986@list.ru>
2026-08-31 14:11:29 +0000

    fix(SortColors): failed example1 test

---
+++ TwoPointers/SortColors/test_solution.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include "Solution.cpp"
+#include <vector>
+using namespace std;
+
+TEST(SolutionTest, example1) {
+  Solution solution;
+  vector<int> nums = {2,0,2,1,1,0};
+  vector<int> expectOutput  = {0,0,1,1,2,2}; 
+
+  solution.sortColors(nums);
+  int size = nums.size();
+  for (int i = 0; i < size; i++) {
+    EXPECT_EQ(nums[i], expectOutput[i]);
+  }
+}
+
