fb4b25876992560b2c240d14a3eb20912b050ccd
Alexandr Anatoliev <per-1986@list.ru>
2026-08-31 16:02:10 +0000

    fix(SortColors): fail test11

--- TwoPointers/SortColors/test_solution.cpp
+++ TwoPointers/SortColors/test_solution.cpp
@@ -99,6 +99,18 @@ TEST(SolutionTest, test10) {
   }
 }
 
+TEST(SolutionTest, test11) {
+  Solution solution;
+  vector<int> nums = {2,0,2,1,1};
+  vector<int> expectOutput  = {0,1,1,2,2}; 
+
+  solution.sortColors(nums);
+  int size = nums.size();
+  for (int i = 0; i < size; i++) {
+    EXPECT_EQ(nums[i], expectOutput[i]);
+  }
+}
+
 TEST(SolutionTest, example1) {
   Solution solution;
   vector<int> nums = {2,0,2,1,1,0};
