c3360f702168aff50c03d37ed56e59798cf945dd
Alexandr Anatoliev <per-1986@list.ru>
2026-09-02 08:22:39 +0000

    fix(MoveZeroes): example1 test failed

---
+++ TwoPointers/MoveZeroes/test_solution.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include "Solution.cpp"
+#include <vector>
+using namespace std;
+
+TEST(SolutionTest, example1) {
+  Solution solution;
+  vector<int> nums = {0,1,0,3,12};
+  vector<int> expectOutput  = {1,3,12,0,0}; 
+
+  solution.moveZeroes(nums);
+  int size = nums.size();
+  for (int i = 0; i < size; i++) {
+    EXPECT_EQ(nums[i], expectOutput[i]);
+  }
+}
+
