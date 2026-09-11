3baa4fa427611d0b004db5b747c61de750d61188
Alexandr Anatoliev <per-1986@list.ru>
2026-09-11 17:58:09 +0000

    fix(RomanToInteger): update charToInt method

--- Strings/RomanToInteger/Solution.cpp
+++ Strings/RomanToInteger/Solution.cpp
@@ -11,7 +11,19 @@ class Solution {
     int charToInt(char ch) {
       switch (ch) {
         case 'I':
-        return 1;
+          return 1;
+        case 'V':
+          return 5;
+        case 'X':
+          return 10;
+        case 'L':
+          return 50;
+        case 'C':
+          return 100;
+        case 'D':
+          return 500;
+        case 'M':
+          return 1000;
       }
       return 0;
     }
